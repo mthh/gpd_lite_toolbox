4006afc6b6688989b1498c1268087d548ac2f246
mthh <mthh@users.noreply.github.com>
2015-10-27 23:31:14 +0100

    Update __init__.py

--- gpd_lite_toolbox/__init__.py
+++ gpd_lite_toolbox/__init__.py
@@ -7,5 +7,6 @@ __version__ = '0.0.0-r27'
 from gpd_lite_toolbox.core import (
     get_borders, find_borders, transform_cartogram, dissolve, intersects_byid,
     multi_to_single, dumb_multi_to_single, snap_to_nearest, read_spatialite, 
-    match_lines, non_contiguous_cartogram, make_grid
+    match_lines, mean_coordinates, non_contiguous_cartogram,
+    make_grid, gridify_data, random_pts_on_surface
     )
