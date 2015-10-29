efe6519d8d50c1742f2c6788877fb241058d9737
mthh <mthh@users.noreply.github.com>
2015-10-29 18:16:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ gpd_lite_toolboox setup file
 
 from distutils.core import setup
 from distutils.extension import Extension
+from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import gpd_lite_toolbox
 
 
-ext = Extension("cycartogram", ["gpd_lite_toolbox/cycartogram.pyx"])
+ext = Extension("gpd_lite_toolbox.cycartogram",
+                ["gpd_lite_toolbox/cycartogram.pyx"], ["."])
 
 setup(
     name='gpd_lite_toolbox',
@@ -17,6 +19,7 @@ setup(
     description='Convenience functions acting on GeoDataFrames',
     author='mthh',
     ext_modules=cythonize(ext),
+    cmdclass = {'build_ext': build_ext},
     packages=['gpd_lite_toolbox'],
     license='MIT',
     )
